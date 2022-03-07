f574ded883885a754a5ad05eae71924bc13323e7
BrightonZhang1 <brighton.zhang23@ycdsbk12.ca>
2022-03-07 17:06:08 -0500

    Final Final Copy of Task 3 (I forgot to add some stuff)

--- Sketch.java
+++ Sketch.java
@@ -8,33 +8,34 @@ public class Sketch extends PApplet {
    */
 
 	float randHouseX = random(0,250);
-    float randHouseY = random(0,275);
+  float randHouseY = random(0,275);
 	
   public void settings() {
     size(400, 400);
   }
 
   public void setup() {
-	// Q2
+  // Colours are assigned to the ground based on the location or quadrant of the top left corner of the house base is in
+	// Q2 Blue
 	if(randHouseX <= 125 && randHouseY <= (275 / 2)) {
     	background(210, 255, 173);
 	}
-	//Q1
+	//Q1 Orange
 	else if(randHouseX >= 125 && randHouseY <= (275 / 2)) {
 		background(168, 109, 50);
 	}
-	//Q3
+	//Q3 Green
 	else if(randHouseX <= 125 && randHouseY >= (275 / 2)) {
 		background(62, 168, 50);
 	}
-	//Q4
+	//Q4 Red
 	else if(randHouseX >= 125 && randHouseY >= (275 / 2)) {
 		background(168, 50, 50);
 	}
   }
 
   public void draw() {
-		
+	// Coordinates used in each function is based on the random variables randHouseX and randHouseY	
 	// Sky
 		fill(52, 223, 235);
 		rect(0, 0, 400, randHouseY + 125);
@@ -51,6 +52,7 @@ public class Sketch extends PApplet {
 		fill(91, 98, 110);
 		triangle(randHouseX - 25, randHouseY, randHouseX + 175, randHouseY, randHouseX + 75, randHouseY - 75);
 	
+  // This code below displays the current time EST in the bottom right corner of the screen
 		var hour = hour();
 		var minute = minute();
 		var second = second();
